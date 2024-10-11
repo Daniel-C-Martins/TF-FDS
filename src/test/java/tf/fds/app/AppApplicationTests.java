ca9638377994a4beee5afec6c70ac3bdefbba775
Daniel Martins <martins.daniel23@edu.pucrs.br>
2024-10-11 19:50:00 -0300

    + inicialização do projeto

---
+++ src/test/java/tf/fds/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package tf.fds.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
