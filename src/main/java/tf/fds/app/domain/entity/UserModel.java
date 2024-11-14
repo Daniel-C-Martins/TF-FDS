a9075a258ea147228af8da059c57499ff22f7d05
Daniel Martins <martins.daniel23@edu.pucrs.br>
2024-11-14 15:19:25 -0300

    Feat: userAdapter implementado com sucesso

--- src/main/java/tf/fds/app/domain/entity/UserModel.java
+++ src/main/java/tf/fds/app/domain/entity/UserModel.java
@@ -1,24 +1,24 @@
 package tf.fds.app.domain.entity;
 
 public class UserModel {
-    private String userName;
+    private String username;
     private String password;
 
-    public UserModel(String userName, String password) {
-        this.userName = userName;
+    public UserModel(String username, String password) {
+        this.username = username;
         this.password = password;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public void setPassword(String password) {
--- src/main/java/tf/fds/app/infra/repositories/adapter/UserAdapter.java
+++ src/main/java/tf/fds/app/infra/repositories/adapter/UserAdapter.java
@@ -3,6 +3,26 @@ package tf.fds.app.infra.repositories.adapter;
 import tf.fds.app.domain.entity.UserModel;
 import tf.fds.app.infra.repositories.entities.User;
 
+
 public class UserAdapter {
-    //Falta popular o User
+    /*
+     * Conversor de UserModel para User
+     * @param usModel
+     * @return User
+     * Cria um objeto User a partir de um objeto UserModel
+     */
+    public static User fromUserModel(UserModel usModel){
+        return new User(usModel.getUsername(), usModel.getPassword());
+    }
+
+    /*
+     * Conversor de User para UserModel
+     * @param user
+     * @return UserModel
+     * Cria um objeto UserModel a partir de um objeto User
+     */
+    public static UserModel toUserModel(User user){
+        return new UserModel(user.getUsername(), user.getPassword());
+    }
+
 }
