2f18add9ca5f424bdb99dfd3c74931931b185388
Gaeltomaz123 <gaeltomaz@gmail.com>
2024-11-18 18:55:41 -0300

    fix: same

--- src/main/java/tf/fds/app/domain/repositories/IApplicativeRepository.java
+++ src/main/java/tf/fds/app/domain/repositories/IApplicativeRepository.java
@@ -3,12 +3,9 @@ package tf.fds.app.domain.repositories;
 import java.util.List;
 
 import tf.fds.app.domain.entities.ApplicativeModel;
-import tf.fds.app.domain.entities.ClientModel;
-import tf.fds.app.domain.entities.SignatureModel.SignatureTypes;
+import tf.fds.app.domain.entities.SignatureModel;
 
 
 public interface IApplicativeRepository {
-    List<ClientModel> getAllClients();
     ApplicativeModel updateMonthlyCost(float newCost, Long id );
-    List<SignatureTypes> getAllSignatures(long codapp);
 }
--- src/main/java/tf/fds/app/domain/repositories/IClientRepository.java
+++ src/main/java/tf/fds/app/domain/repositories/IClientRepository.java
@@ -6,5 +6,4 @@ import tf.fds.app.domain.entities.ClientModel;
 
 public interface IClientRepository {
     List<ClientModel> getAllClients();
-
 }
