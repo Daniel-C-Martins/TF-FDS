12c87300a66a7b9aecfdd6c3c68fcc828925b037
Gabriel de Cezaro Tomaz <66495941+Gaeltomaz123@users.noreply.github.com>
2024-11-21 05:50:17 +0000

    feat: Criação do script de Inserção de dados (assinatura não está funcionando)

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,26 @@
+--10 clientes, 5 aplicativos, 5 assinaturas ativas
+-- Clientes
+INSERT INTO client (name, email) VALUES ('Antonio Carlos', 'tomjobim@gmail.com');
+INSERT INTO client (name, email) VALUES ('Jorge Ben', 'jorgeben@gmail.com');
+INSERT INTO client (name, email) VALUES ('Joao Gilberto', 'joaogilberto@gmail.com');
+INSERT INTO client (name, email) VALUES ('Vinicius de Moraes', 'vmoraes@outlook.com');
+INSERT INTO client (name, email) VALUES ('Rita Lee', 'ritalee@outlook.com');
+INSERT INTO client (name, email) VALUES ('Elis Regina', 'elis@outlook.com');
+INSERT INTO client (name, email) VALUES ('Angenor de Oliveira', 'cartola@gmail.com');
+INSERT INTO client (name, email) VALUES ('Cassia Eller', 'cassia@yahoo.com');
+INSERT INTO client (name, email) VALUES ('Gal Costa', 'galcosta@yahoo.com');
+INSERT INTO client (name, email) VALUES ('Sebastião Rodrigues Maia', 'timmaia@yahoo.com');
+
+
+
+
+-- Aplicativos
+INSERT INTO applicative (name, monthly_cost) VALUES ('Spotify', 29.99);
+INSERT INTO applicative (name, monthly_cost) VALUES ('SoundClound', 59.99);
+INSERT INTO applicative (name, monthly_cost) VALUES ('Audacity', 199.99);
+INSERT INTO applicative (name, monthly_cost) VALUES ('GarageBand', 249.99);
+INSERT INTO applicative (name, monthly_cost) VALUES ('Deezer', 19.99);
+
+
+-- Assinaturas
+-- INSERT INTO signature (beginning_Date, end_date, client_id, applicative_id) VALUES ('2024-11-16', '2024-12-16', 1, 1);
